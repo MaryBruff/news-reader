1b80f62cac5edbcff5b84e4790fab833c1a9884e
Mary Bruff <marybruff5@gmail.com>
2024-01-20 10:12:32 -0700

    add api calls and sample data

---
+++ src/components/Api-calls.js
@@ -0,0 +1,9 @@
+export default async function getArticles() {
+  const apiKey = process.env.REACT_APP_API_KEY;
+  return await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`)
+  .then(response => response.json())
+  .then(data => {return data})
+  .catch(error => console.log(error))
+}  
+
+
