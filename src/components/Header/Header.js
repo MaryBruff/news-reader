c37e790e856043b9b5278cf0c46115b18b229b4d
Mary Bruff <marybruff5@gmail.com>
2024-01-21 17:30:56 -0700

    add back button to article card

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,31 +1,40 @@
-// import React from 'react'
 import "./Header.css";
 import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 
-const Header = ({ searchInput, setSearchInput }) => {
+const Header = ({ searchInput, setSearchInput, resetSearchInput, showBackButton }) => {
   const [searchArticle, setSearchArticle] = useState("");
+
+  const navigate = useNavigate();
 
   const handleSearch = (event) => {
     event.preventDefault();
-    setSearchInput(searchArticle);
+    setSearchInput(searchArticle.toLowerCase());
     setSearchArticle("");
   };
+
+  const goBack = () => {
+    navigate('/'); // Navigate back to the homepage
+  };
+
   return (
     <header className="vintage-header">
-      <h1>Ye Olde Shoppe</h1>
-
+    <h1 className="vintage-header-title">Top Story Gazette</h1>
       <div className="search-container">
         <form onSubmit={handleSearch}>
           <input
             className="search-bar"
             type="text"
-            placeholder="Search Articles"
+            placeholder="Search by Source"
             value={searchArticle}
             onChange={(event) => setSearchArticle(event.target.value)}
           />
           <button className="search-button" type="submit">
             Search
           </button>
+          {showBackButton && (
+            <button className="go-back" onClick={goBack}>Back</button>
+          )}
         </form>
       </div>
     </header>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,11 +3,15 @@ import './Home.css'
 import ArticleContainer from '../ArticleContainer/ArticleContainer'
 import Header from '../Header/Header'
 
-const Home = ({ filterArticles, searchInput, setSearchInput }) => {
+const Home = ({ filterArticles, searchInput, setSearchInput, resetSearchInput }) => {
 
   return (
     <main className="home">
-      <Header searchInput={searchInput} setSearchInput={setSearchInput} />
+      <Header 
+      searchInput={searchInput} 
+      setSearchInput={setSearchInput}
+      resetSearchInput={resetSearchInput}
+       />
       <ArticleContainer articles={filterArticles()} />
     </main>
   )
